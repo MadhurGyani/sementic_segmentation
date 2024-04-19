74e2c2279c175b47d5000e3eaf77467240ea93b2
MadhurGyani <137261792+MadhurGyani@users.noreply.github.com>
2024-04-19 05:58:31 +0530

    Add files via upload

---
+++ 000project1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+{
+    int fav_num;
+    std::cout <<"enter your favourite no. between 1 to 100";
+    std::cin >>fav_num;
+    std::cout <<"amazing thats my fav num too" << std::endl;
+    return 0;
+}
